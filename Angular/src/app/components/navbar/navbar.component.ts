0b68361ba4c88014c6b3099534450844f40869b1
Isar <isar.arnautovic16@gmail.com>
2025-11-30 18:24:54 +0100

    TASK-6: Enable sticky positioning for Navbar on scroll
    - Updated Navbar CSS to remain fixed at the top of the viewport when scrolling.

--- Angular/src/app/components/navbar/navbar.component.ts
+++ Angular/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
@@ -11,7 +11,14 @@ import { AuthService } from '../../services/auth.service';
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  isScrolled = false;
+
   constructor(public authService: AuthService) {}
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.isScrolled = window.scrollY > 10;
+  }
 
   logout(): void {
     this.authService.logout();
