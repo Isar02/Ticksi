9bbf088d0400b88a77013fb02277a8cd0df1e664
Isar <isar.arnautovic16@gmail.com>
2025-12-17 20:13:53 +0100

    TASK-20: repository layer - interface and implementation

---
+++ Backend/Ticksi.Application/Interfaces/IEventRepository.cs
@@ -0,0 +1,15 @@
+using Ticksi.Application.DTOs;
+using Ticksi.Domain.Entities;
+
+namespace Ticksi.Application.Interfaces
+{
+    public interface IEventRepository
+    {
+        Task<PagedResult<Event>> GetPagedEventsAsync(EventQueryDto query);
+        Task<Event?> GetByPublicIdAsync(Guid publicId);
+        Task AddAsync(Event eventEntity);
+        Task UpdateAsync(Event eventEntity);
+        Task DeleteAsync(Event eventEntity);
+    }
+}
+
