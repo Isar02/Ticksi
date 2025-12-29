11b0c52da15104a27d7bed57d0a9befbc92506ab
Sedad <sedadtinjak@gmail.com>
2025-12-29 17:04:07 +0100

    TASK-33: Implement Predictive Search Suggestions Endpoint

---
+++ Backend/Ticksi.Application/DTOs/SearchSuggestionDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ticksi.Application.DTOs
+{
+    public sealed class SearchSuggestionDto
+    {
+
+        public Guid PublicId { get; set; }          // <-- BITNO: Guid, ne string
+        public string Type { get; set; } = "";      // "event" | "location" | "category"
+        public string Label { get; set; } = "";     // šta prikazuješ u UI
+        public double Score { get; set; }              // za ranking
+    }
+}
